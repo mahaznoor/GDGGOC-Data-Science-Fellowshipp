fa459c7774ac2f67d8d5b5d050d2ca681763edfa
Mahaz noor <mahaznoori@gmail.com>
2026-03-27 22:48:21 +0500

    Task3

--- WEEK 1/Task3/Task3/bank_account.py
+++ WEEK 1/Task3/Task3/bank_account.py
@@ -37,6 +37,6 @@ class CheckingAccount(bankAccount):
             self.balance-=amount
             print(f"Withdrew {amount}. New balance is {self.balance}")
 
-savings= SavingsAccount("AN123","Anees",100000)
-savings.deposit(5000)
-savings.withdraw(2000)
+savings= SavingsAccount("123","mahaz noor",10000)
+savings.deposit(500)
+savings.withdraw(200)
